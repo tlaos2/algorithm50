1ed274efebd7dbf5a870204357b897282bedfb3d
SSUNGOK <>
2021-08-12 14:07:07 +0900

    max diff

--- B10819.js
+++ B10819.js
@@ -4,27 +4,44 @@ let nums = [20, 1, 15, 8, 4, 6];
 // console.log(nums);
 let i = 0;
 let arrRtn = [];
+let maxDiff = 0;
 nums.forEach((item, idx) => {
   console.log(item, idx);
   let temp = [...nums];
   let fixednum = temp.splice(idx, 1);
-  getPermutation(temp, [fixednum]);
-  // arrRtn.push(getPermutation(temp, [fixednum]));
+  getPermutation(temp, [fixednum]);  
 });
 
 function getPermutation(arr, result) {
   if (arr.length === 1) {
-    console.log(++i, [...result, arr[0]].join(','));
+    // console.log(++i, [...result, arr[0]].join(','));
+    let rtnArr =[ ...result.flat(), arr[0]]; 
+    let diff = getDiff(rtnArr);
+    if(getDiff(rtnArr) > maxDiff){
+      maxDiff = diff;
+      console.log('MAX DIFF :' , diff,"-" ,  [...result, arr[0]].join(','));
+    }
 
-    arrRtn.push([...result, arr[0]]);
-    return [...result, arr[0]];
+    arrRtn.push(rtnArr);
+    return [ ...result.flat(), arr[0]];
   } else {
     for (let i = 0; i < arr.length; i++) {
       let tmp = [...arr];
       let fixednum = tmp.splice(i, 1);
-      getPermutation(tmp, [...result, fixednum]);
+      getPermutation(tmp, [result.flat(), fixednum]);
     }
   }
 }
+
+function getDiff(arr){
+
+  let rtn = 0;
+  for(let i=0; i < arr.length -1; i++){
+    rtn += Math.abs(arr[i]-arr[i+1]);
+  }
+
+  return rtn;
+}
+
 
 console.log(arrRtn);
