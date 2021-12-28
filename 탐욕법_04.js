bf38f7b005785ca123d87aeb4a82b7a2a28d2087
IamGrooot <tlaos2@naver.com>
2021-12-29 00:05:46 +0900

    탐욕4

---
+++ 탐욕법_04.js
@@ -0,0 +1,15 @@
+function solution(routes) {
+  var answer = 0;
+
+  //   console.log(routes);
+
+  return answer;
+}
+
+let in1 = [
+  [-20, -15],
+  [-14, -5],
+  [-18, -13],
+  [-5, -3],
+];
+console.log(solution(in1));
